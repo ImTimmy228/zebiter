25e689c323e2f37136ff3c4298bc5758fa240536
ImTimmy228 <74459442+ImTimmy228@users.noreply.github.com>
2024-11-26 18:12:10 +0500

    Create main.lua

---
+++ src/main.lua
@@ -0,0 +1,286 @@
+-- Credits to edgeiy (Infinite Yield 6.1)
+ -- Credits to edgeiy (Skyhub)
+
+-- TODO
+-- Fork all scripts to my own repository incase of any bad situations
+
+
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/Skzuppy/forge-hub/main/loader.lua"))() [Good for Fisch, HARD Checkpoints]
+-- loadstring(game:HttpGet("https://norepinefrina.com"))() [Best Gold+(ingots) Autofarm, Checkpoints.] / loadstring(game:HttpGet("https://raw.githubusercontent.com/norepinefrina/i/main/sursa"))() [No popup] 
+
+
+getgenv().Debiter = false -- False to STOP debug mode, disallowing executing twice
+
+if biterran and not Debiter == true then -- If ran and debug mode isn't enabled, then quit.
+    -- error("biter is already running!", 0)
+    return
+end
+
+pcall(function() getgenv().biterran = true end) -- Set variable of script being ran to true
+
+local cloneref = cloneref or function(o) return o end -- Just took it for no reason lol
+COREGUI = cloneref(game:GetService("CoreGui"))
+Players = cloneref(game:GetService("Players"))
+
+if not game:IsLoaded() then -- Wait for game to be loaded
+    game.Loaded:Wait()
+end
+
+currentVersion = "0"
+
+
+
+
+
+
+queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
+httprequest = (syn and syn.request) or (http and http.request) or http_request or (fluxus and fluxus.request) or request
+HttpService = game:GetService("HttpService")
+everyClipboard = setclipboard or toclipboard or set_clipboard or setrbxclipboard or (Clipboard and Clipboard.set)
+if not everyClipboard then
+	-- creds to vxsty
+getgenv().setclipboard = function(data)
+    local vim = game:GetService('VirtualInputManager');
+    local old = game:GetService("UserInputService"):GetFocusedTextBox()
+    local copy = tostring(data)
+    local gui = Instance.new("ScreenGui", getgenv().gethui())
+    local a = Instance.new('TextBox', gui)
+    a.PlaceholderText = ''
+    a.Text = copy
+    a.ClearTextOnFocus = false
+    a.Size = UDim2.new(.1, 0, .15, 0)
+    a.Position = UDim2.new(10, 0, 10, 0)
+    a:CaptureFocus()
+    a = Enum.KeyCode
+    local Keys = {
+     a.RightControl, a.A
+    }
+    local Keys2 = {
+     a.RightControl, a.C, a.V
+    }
+    for i, v in ipairs(Keys) do
+     vim:SendKeyEvent(true, v, false, game)
+     task.wait()
+    end
+    for i, v in ipairs(Keys) do
+     vim:SendKeyEvent(false, v, false, game)
+     task.wait()
+    end
+    for i, v in ipairs(Keys2) do
+     vim:SendKeyEvent(true, v, false, game)
+     task.wait()
+    end
+    for i, v in ipairs(Keys2) do
+     vim:SendKeyEvent(false, v, false, game)
+     task.wait()
+    end
+    gui:Destroy()
+    if old then old:CaptureFocus() end
+end
+-- creds to vxsty
+getgenv().setrbxclipboard = function(data)
+    local vim = game:GetService('VirtualInputManager');
+    local old = game:GetService("UserInputService"):GetFocusedTextBox()
+    local copy = tostring(data)
+    local gui = Instance.new("ScreenGui", getgenv().gethui())
+    local a = Instance.new('TextBox', gui)
+    a.PlaceholderText = ''
+    a.Text = copy
+    a.ClearTextOnFocus = false
+    a.Size = UDim2.new(.1, 0, .15, 0)
+    a.Position = UDim2.new(10, 0, 10, 0)
+    a:CaptureFocus()
+    a = Enum.KeyCode
+    local Keys = {
+     a.RightControl, a.A
+    }
+    local Keys2 = {
+     a.RightControl, a.C, a.V
+    }
+    for i, v in ipairs(Keys) do
+     vim:SendKeyEvent(true, v, false, game)
+     task.wait()
+    end
+    for i, v in ipairs(Keys) do
+     vim:SendKeyEvent(false, v, false, game)
+     task.wait()
+    end
+    for i, v in ipairs(Keys2) do
+     vim:SendKeyEvent(true, v, false, game)
+     task.wait()
+    end
+    for i, v in ipairs(Keys2) do
+     vim:SendKeyEvent(false, v, false, game)
+     task.wait()
+    end
+    gui:Destroy()
+    if old then old:CaptureFocus() end
+end
+-- creds to vxsty
+getgenv().toclipboard = function(data)
+    local vim = game:GetService('VirtualInputManager');
+    local old = game:GetService("UserInputService"):GetFocusedTextBox()
+    local copy = tostring(data)
+    local gui = Instance.new("ScreenGui", getgenv().gethui())
+    local a = Instance.new('TextBox', gui)
+    a.PlaceholderText = ''
+    a.Text = copy
+    a.ClearTextOnFocus = false
+    a.Size = UDim2.new(.1, 0, .15, 0)
+    a.Position = UDim2.new(10, 0, 10, 0)
+    a:CaptureFocus()
+    a = Enum.KeyCode
+    local Keys = {
+     a.RightControl, a.A
+    }
+    local Keys2 = {
+     a.RightControl, a.C, a.V
+    }
+    for i, v in ipairs(Keys) do
+     vim:SendKeyEvent(true, v, false, game)
+     task.wait()
+    end
+    for i, v in ipairs(Keys) do
+     vim:SendKeyEvent(false, v, false, game)
+     task.wait()
+    end
+    for i, v in ipairs(Keys2) do
+     vim:SendKeyEvent(true, v, false, game)
+     task.wait()
+    end
+    for i, v in ipairs(Keys2) do
+     vim:SendKeyEvent(false, v, false, game)
+     task.wait()
+    end
+    gui:Destroy()
+    if old then old:CaptureFocus() end
+end
+end
+if not cloneref then
+	getgenv().cloneref = function(a)
+		local s, _ = pcall(function() return a:Clone() end) return s and _ or a
+	end
+end
+HttpService = cloneref(game:GetService("HttpService")) or game:GetService("HttpService")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "ze biter                              Updates: " .. currentVersion,
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "made for solara",
+   LoadingSubtitle = "im poor as fuck",
+   Theme = "Serenity", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+
+   DisableRayfieldPrompts = true,
+   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+
+   ConfigurationSaving = {
+      Enabled = false
+   },
+
+   KeySystem = true, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "for Skids",
+      Subtitle = "bitr",
+      Note = "Fuck yourself", -- Use this to tell the user how to get a key
+      FileName = "passwords", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"52"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
+
+local Games = Window:CreateTab("Games", "gamepad-2") -- Title, Image
+local Universal = Window:CreateTab("Universal", "gamepad-2") -- Title, Image
+local Button = Games:CreateButton({
+   Name = "Fisch",
+   Callback = function()
+   -- The function that takes place when the button is pressed
+   end,
+})
+
+
+
+local Section = Universal:CreateSection("Random")
+local Button = Games:CreateButton({
+   Name = "Sky hub",
+   Callback = function()
+   loadstring(game:HttpGet('https://raw.githubusercontent.com/yofriendfromschool1/Sky-Hub/main/SkyHub.txt'))()
+   end,
+})
+
+
+local Button = Games:CreateButton({
+   Name = "Debugger Sky hub",
+   Callback = function()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/yofriendfromschool1/debugnation/main/decompilers%20and%20debugging/Debuggers.txt"))()
+   end,
+})
+
+local Section = Universal:CreateSection("Admins")
+local Button = Universal:CreateButton({
+   Name = "Nameless Admin",
+   Callback = function()
+   loadstring(game:HttpGet('https://raw.githubusercontent.com/yofriendfromschool1/NamelessAdmin-NO-BYFRON-GUI/main/Source'))()
+   end,
+})
+
+local Button = Universal:CreateButton({
+   Name = "'random' cmd Admin",
+   Callback = function()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/lxte/cmd/main/testing-main.lua"))()
+   end,
+})
+
+local Button = Universal:CreateButton({
+   Name = "Infinite Yield",
+   Callback = function()
+   loadstring(game:HttpGet('https://raw.githubusercontent.com/edgeiy/infiniteyield/master/source'))()
+   end,
+})
+
+local Button = Universal:CreateButton({
+   Name = "ShatterVast",
+   Callback = function()
+   loadstring(game:HttpGet('https://raw.githubusercontent.com/yofriendfromschool1/Sky-Hub-Backup/main/%5BFE%5D%20Shattervast.lua'))()
+   end,
+})
+
+local Button = Universal:CreateButton({
+   Name = "Proton Admin OBFUSCATED",
+   Callback = function()
+	_G.UI_Id = "default" --set this to "default" for the default ui
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/DigitalityScripts/roblox-scripts/main/Proton%20Free'))()
+   end,
+})
+
+local Button = Universal:CreateButton({
+   Name = "Proton Free 2 OBFUSCATED",
+   Callback = function()
+	_G.UI_Id = "default" --set this to "default" for the default ui
+	loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/5e6e6cc1bb32fd926764d064e2c60a3b.lua"))()
+   end,
+})
+
+local Button = Universal:CreateButton({
+   Name = "Reviz Admin V2",
+   Callback = function()
+	_G.UI_Id = "default" --set this to "default" for the default ui
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/yofriendfromschool1/Sky-Hub/main/Reviz%20AdminV2"))()
+   end,
+})
